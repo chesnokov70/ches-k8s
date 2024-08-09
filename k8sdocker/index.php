6fd966824ce620079bc20e9e7c4b3eedbf38963c
Sergei Chesnokov <ches.tranveks@gmail.com>
2024-08-08 22:56:18 -0400

    added Lesson__8

---
+++ k8sdocker/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hello from Kubernetes</h1><br>";
+echo "<h3>Server IP Address is: $ip_server</h3>", "<br><p>";
+echo "Made by <font color=magenta>Sergei Chesnokov";
+
+
+?>
